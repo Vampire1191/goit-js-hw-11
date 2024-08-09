934e25d0cbc26729b96b3011f933db9b4678ae7b
Vampire1191 <Serzhgrebinevich@gmail.com>
2024-08-09 22:36:58 +0200

    first
    перша спроба і перевірка загальної роботоспособності

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const apiKey = '45360180-cc3ae961e731e1f4ede944c53';
+export async function fetchImages(searchTerm) {
+  const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
+    searchTerm
+  )}&image_type=photo&orientation=horizontal&safesearch=true&order=popular&per_page=9`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.hits;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw new Error('Failed to fetch images from Pixabay');
+  }
+}
